8701133077b37e60c6f28bf0de149f346ff1a406
Amber Taranto <ambzzzz@gmail.com>
2019-08-28 00:48:28 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -7,15 +7,15 @@ CREATE TABLE series (
 
 CREATE TABLE subgenres (
     id INTEGER PRIMARY KEY,
-    name TEXT
+    name TEXT,
 );
 
 CREATE TABLE authors (
     id INTEGER PRIMARY KEY,
-    name TEXT
+    name TEXT,
 );
 
 CREATE TABLE books (
     id INTEGER PRIMARY KEY,
-    name TEXT
+    name TEXT,
 );
